19a66c6aa3f08bce57b63b5d522f0cd0c64934ad
Robert Coup <robert@coup.net.nz>
2017-10-06 11:00:07 +0100

    Fix for parsing GEOS version. The actual fix is in 2.0 as 78c155cf (Upstream #28441), but this makes fewer changes.

--- django/contrib/gis/geos/libgeos.py
+++ django/contrib/gis/geos/libgeos.py
@@ -172,9 +172,10 @@ geos_version = GEOSFuncFactory('GEOSversion', restype=c_char_p)
 
 # Regular expression should be able to parse version strings such as
 # '3.0.0rc4-CAPI-1.3.3', '3.0.0-CAPI-1.4.1', '3.4.0dev-CAPI-1.8.0' or '3.4.0dev-CAPI-1.8.0 r0'
+# or '3.6.2-CAPI-1.10.2 4d2925d6'
 version_regex = re.compile(
     r'^(?P<version>(?P<major>\d+)\.(?P<minor>\d+)\.(?P<subminor>\d+))'
-    r'((rc(?P<release_candidate>\d+))|dev)?-CAPI-(?P<capi_version>\d+\.\d+\.\d+)( r\d+)?$'
+    r'((rc(?P<release_candidate>\d+))|dev)?-CAPI-(?P<capi_version>\d+\.\d+\.\d+)'
 )
 
 
