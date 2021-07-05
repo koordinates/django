1a342689702a56e6b37db5e676b48cd1f43339c0
Craig de Stigter <craig@destigter.nz>
2021-07-06 11:54:44 +1200

    update release instructions for our fork

--- django/utils/version.py
+++ django/utils/version.py
@@ -82,7 +82,7 @@ def get_git_changeset():
     git_log = subprocess.run(
         ['git', 'log', '--pretty=format:%ct', '--quiet', '-1', 'HEAD'],
         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-        shell=True, cwd=repo_dir, universal_newlines=True,
+        cwd=repo_dir, universal_newlines=True,
     )
     timestamp = git_log.stdout
     try:
