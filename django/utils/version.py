760d8f90769336feb6afd2a02c99bc8a891af058
Craig de Stigter <craig@destigter.nz>
2021-08-17 16:05:31 +1200

    Allow for pacakged "dev" versions; no git present

--- django/utils/version.py
+++ django/utils/version.py
@@ -34,14 +34,16 @@ def get_version(version=None):
     main = get_main_version(version)
 
     sub = ""
+    mapping = {"alpha": "a", "beta": "b", "rc": "rc"}
     if version[3] == "alpha" and version[4] == 0:
         git_changeset = get_git_changeset()
         if git_changeset:
             sub = ".dev%s" % git_changeset
 
-    elif version[3] != "final":
-        mapping = {"alpha": "a", "beta": "b", "rc": "rc"}
+    elif version[3] in mapping:
         sub = mapping[version[3]] + str(version[4])
+    else:
+        return f"{main}.{version[3]}{version[4] or ''}"
 
     return main + sub
 
@@ -62,7 +64,6 @@ def get_complete_version(version=None):
         from django import VERSION as version
     else:
         assert len(version) == 5
-        assert version[3] in ("alpha", "beta", "rc", "final")
 
     return version
 
